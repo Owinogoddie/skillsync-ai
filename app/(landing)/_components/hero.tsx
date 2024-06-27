6aec167a174e7089a76c2657cd749c157fc7dec2
Owinogoddie <owinogoddie@gmail.com>
2024-06-27 16:54:49 +0300

    modifying hero

--- app/(landing)/_components/hero.tsx
+++ app/(landing)/_components/hero.tsx
@@ -1,10 +1,15 @@
 // src/_components/Hero.tsx
-import React from 'react';
-import { motion } from 'framer-motion';
-import ScrollAnimation from '../animations/scroll-animation';
-import Link from 'next/link';
+import React from "react";
+import { motion } from "framer-motion";
+import ScrollAnimation from "../animations/scroll-animation";
+import Link from "next/link";
+import { SignInButton, UserButton, useAuth } from "@clerk/nextjs";
+import { CourseModal } from "./course-modal";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 const Hero: React.FC = () => {
+  const { isLoaded, userId } = useAuth();
   return (
     <header className="bg-gradient-to-br from-gradient-start to-gradient-end text-white py-32">
       <div className="container mx-auto text-center px-4">
@@ -19,12 +24,27 @@ const Hero: React.FC = () => {
           </p>
         </ScrollAnimation>
         <ScrollAnimation direction="up">
+
           <motion.button
             className="bg-primary text-white px-8 py-3 rounded-full font-semibold text-lg hover:bg-primary-dark transition duration-300"
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            <Link href="/dashboard">Get Started Free</Link>
+            {userId ? (
+              
+            <Link href="/dashboard/autogen">Get Started Free</Link>
+              
+            ) : (
+              <>
+                {!isLoaded ? (
+                  <Loader2 className="animate-spin h-4 w-4" />
+                ) : (
+                  <SignInButton mode="modal">
+                      SignIn
+                  </SignInButton>
+                )}
+              </>
+            )}
           </motion.button>
         </ScrollAnimation>
       </div>
